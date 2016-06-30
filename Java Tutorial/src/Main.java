c119e2e010852a8e85f488f1c8bfeabe9dbc721e
Zuhab Wasim <z-wasim-786@live.com>
2016-06-30 18:34:44 -0400

    added main

--- Java Tutorial/src/Main.java
+++ Java Tutorial/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World");
+		System.out.println("Conditional Structures")
+		
+		int j = 10
+		if (j == 10) {
+			
+		}
 	}
 
 }
